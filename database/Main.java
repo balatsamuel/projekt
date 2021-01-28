fb816b8ba9a88d92cc66c51f1c4768783542bd9e
Samo Balát <balatsamuel2001@gmail.com>
2021-01-28 20:30:08 +0100

    tretí commit

--- database/Main.java
+++ database/Main.java
@@ -6,12 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
 
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("database/Databaza.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("database/Databaza.fxml")));
         primaryStage.setTitle("Databaza Knižnice");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
