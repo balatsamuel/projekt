e22414b09b5ce5389fe38a97bdfffe61d688a63c
Samo Balát <balatsamuel2001@gmail.com>
2021-02-13 16:15:44 +0100

    5 commit

---
+++ NájtKnižku/NájstKnižkuMain.java
@@ -0,0 +1,26 @@
+package NájtKnižku;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class NájstKnižkuMain extends Application {
+
+        @Override
+        public void start(Stage primaryStage) throws Exception{
+            Parent root = FXMLLoader.load(getClass().getResource("NájstKnižku.fxml"));
+            primaryStage.setTitle("Vyhladať Knihu");
+            primaryStage.setScene(new Scene(root, 600, 400));
+            primaryStage.show();
+        }
+
+
+        public static void main(String[] args) {
+            launch(args);
+        }
+
+    }
+
+}
---
+++ NájtKnižku/NájsťKnižkucontroller.java
@@ -0,0 +1,4 @@
+package NájtKnižku;
+
+public class NájsťKnižkucontroller {
+}
